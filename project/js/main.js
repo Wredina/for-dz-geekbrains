1513bf8af1205a3f4d8088002b1d17f4d1178ea3
Sofiya <sofiya1989@inbox.ru>
2022-02-16 23:33:33 +0300

    homework

---
+++ project/js/main.js
@@ -0,0 +1,37 @@
+const products = [
+  {id: 1, title: 'Notebook', price: 2000},
+  {id: 2, title: 'Mouse', price: 20},
+  {id: 3, title: 'Keyboard', price: 200},
+  {id: 4, title: 'Gamepad', price: 50},
+];
+
+const renderProduct = (itemEl) => {
+  return `<div class="product-item">
+              <h3>${itemEl.title}</h3>
+              <p>${itemEl.price}</p>
+              <button class="buy-btn">Купить</button>
+          </div>`
+};
+const renderPage = list => {
+  // запятая после каждого товара из-за того, что по умолчанию масивы
+  // отделяются друг от друга запятой. Поэтому нужно либо объеденить
+  // массивы, либо перевести их в строки. 
+  const productsList = list.map(item => renderProduct(item)).join('');//вариант один
+  // вариант два (но он длинее)
+  // let productArr = '';
+  // for (let i = 1; i < productsList.length; i++){
+  //    productArr += productsList[0].concat(productsList[i]);
+  // };
+  // console.log (productArr);
+  console.log(productsList);
+  document.querySelector('.products').innerHTML = productsList; // productArr
+};
+
+renderPage(products);
+
+// 1. Добавьте стили для верхнего меню, товара, списка товаров и кнопки
+//  вызова корзины.
+// 2. Добавьте значения по умолчанию для аргументов функции. 
+// Как можно упростить или сократить запись функций?
+// 3. *Сейчас после каждого товара на странице выводится запятая.
+//  Из-за чего это происходит? Как это исправить? 
